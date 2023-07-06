5e3bb1cd275ce84accc5a4ca7ddd77462012d870
ZE-R0-1 <jijiij77@naver.com>
2023-07-06 18:16:20 +0900

    Initial Commit

---
+++ zeroMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  zeroMemo
+//
+//  Created by 윤준영 on 2023/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
