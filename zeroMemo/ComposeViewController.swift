c74c23d215167790a6c55eafc2a0d1cdc8d7210c
ZE-R0-1 <jijiij77@naver.com>
2023-07-06 20:47:25 +0900

    취소 버튼 구현

--- zeroMemo/ComposeViewController.swift
+++ zeroMemo/ComposeViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ComposeViewController: UIViewController {
 
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
